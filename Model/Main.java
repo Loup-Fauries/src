bfa1a35c4235351b52db97ad0e80c09b7f2bcec2
Loup-Fauries <loup.fauries@gmail.com>
2018-02-18 00:17:48 +0100

    Début JavaDoc

--- Model/Main.java
+++ Model/Main.java
@@ -3,13 +3,26 @@ package Model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Classe représentant la main d'un joueur
+ * 
+ * @version: 1.0
+ */
 public class Main {
 	private List<Carte> main = new ArrayList<Carte>();
 	
+	/**
+	 * Ajoute une carte à la main
+	 * 
+	 */
 	public void ajouter(Carte carte) {
 		main.add(carte);
 	}
 	
+	/**
+	 * Retourne la valeur de la main
+	 * 
+	 */
 	public int cartesValeur(){
 		int valeur = 0;
 		int nbas = 0;
@@ -30,6 +43,10 @@ public class Main {
 		return valeur;
 	}
 	
+	/**
+	 * Forme une chaine de caractères constituée de la main d'un joueur
+	 * 
+	 */
 	public String toString(){
 		String listedecarte = "";
 		for(Carte carte : main){
@@ -38,10 +55,18 @@ public class Main {
 		return listedecarte;
 	}
 	
+	/**
+	 * Retourne le nombre de carte dans main
+	 * 
+	 */
 	public int tailleMain(){
 		return main.size();
 	}
 	
+	/**
+	 * Retourne les cartes de la main face visible
+	 * 
+	 */
 	public void retournerCartes() {
 		for (Carte carte : main) {
 			if(!carte.estVisible()) {
@@ -50,6 +75,10 @@ public class Main {
 		}
 	}
 	
+	/**
+	 * Retourne true si la main vaut : Blackjack
+	 * 
+	 */
 	public boolean isBlackjack() {
 		return (tailleMain() == 2 && cartesValeur() == 21);
 	}
