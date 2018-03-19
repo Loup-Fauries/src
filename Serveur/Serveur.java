2b1692c582a772c8d9715f1196d66c3d349c3b18
LeRoixBleu <loup.fauries@gmail.com>
2018-03-19 17:47:50 +0100

    Add files via upload

--- Serveur/Serveur.java
+++ Serveur/Serveur.java
@@ -1,16 +1,25 @@
 package Serveur;
 
 import java.io.IOException;
-import java.net.*;
 
+import java.net.*;
+/**
+ * Classe représentant le serveur(Casino)
+ * 
+ * @version: 1.0
+ */
 public class Serveur {
 
+	/**
+	 * Ouverture de socket puis attente des clients
+	 * 
+	 */
 	public static void main(String[] zero){
 		
 		ServerSocket socket;
 		try {
 			socket = new ServerSocket(2009);
-			Thread t = new Thread(new Accepter_clients(socket));
+			Thread t = new Thread(new Accepter_clients(socket, 2));
 			t.start();
 			System.out.println("Le Casino (serveur centrale) est prêt !");
 		
